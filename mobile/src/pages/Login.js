7d9dc8ec89b638f9ae2b20cd2815bf7ba2653780
Damaso Junio <junio.pereirab@gmail.com>
2021-01-03 18:55:53 -0300

    Creating login page on Mobile

---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+import logo from '../assets/logo.png';
+
+function Login() {
+  return (
+    <View style={styles.container}>
+      <Image source={logo} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Digite seu usuario no GitHub"
+        placeholderTextColor="#999999"
+        style={styles.input}
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Enviar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
+  },
+  input: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#FFFFFF',
+    borderWidth: 1,
+    borderColor: '#DDDDDD',
+    borderRadius: 4,
+    marginTop: 20,
+    paddingHorizontal: 15,
+  },
+  button: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#DF4723',
+    borderRadius: 4,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
+
+export default Login;
